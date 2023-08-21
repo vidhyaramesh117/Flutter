077ff9f8b46c66db3afac651cfb4889ef3bb3157
Vidhya <vidhyaramesh117@gmail.com>
2023-08-21 15:45:44 +0500

    scaffold full project

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget
 
 class MyHomePageState extends State<MyHomePage> 
 {
-  int count = 0;
+  int index_of_item = 0,count = 0;
 
   @override
   Widget build(BuildContext context) 
@@ -37,6 +37,15 @@ class MyHomePageState extends State<MyHomePage>
    
     return Scaffold
     (
+
+//primary
+      primary: true,
+
+//resize to avoid bottom set
+      resizeToAvoidBottomInset: true,
+
+//background color
+      backgroundColor: Colors.white,
 
 //appbar      
       appBar: AppBar
@@ -49,17 +58,6 @@ class MyHomePageState extends State<MyHomePage>
       (
         
       ),
-
-//floating action button
-      floatingActionButton: FloatingActionButton
-      (
-        onPressed: () => setState(() 
-        {
-          null;
-        }),
-        child: Icon(Icons.message),
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
 
 //drawer
       drawer: Drawer
@@ -102,7 +100,7 @@ class MyHomePageState extends State<MyHomePage>
             ListTile
             (
               title: const Text("Profile"),
-              leading: const Icon(Icons.person),
+              leading: const Icon(Icons.person_3),
 
               onTap: ()
               {
@@ -123,6 +121,172 @@ class MyHomePageState extends State<MyHomePage>
           ],
         )
       ),
+
+//end drawer
+      // endDrawer: Drawer
+      // (
+      //   child: ListView
+      //   (
+      //     children: <Widget>
+      //     [
+      //       const DrawerHeader
+      //       (
+      //         decoration: BoxDecoration
+      //         (
+      //           color: Colors.blue,
+      //         ),
+      //         child: UserAccountsDrawerHeader(
+      //           decoration: BoxDecoration(color: Colors.blue),
+      //           accountName: Text
+      //           (
+      //             "Extreme Psycho",
+      //             style: TextStyle(fontSize: 20),
+      //           ), 
+      //           accountEmail: Text("9072381972"),
+      //           currentAccountPictureSize: Size.square(50),
+      //           currentAccountPicture: CircleAvatar
+      //           (
+      //             backgroundColor: Color.fromARGB(255, 45, 175, 226),
+      //             child: Text
+      //             (
+      //               "EP",
+      //               style: TextStyle
+      //               (
+      //                 fontSize: 30.0,
+      //                 color: Colors.black
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+
+      //       ListTile
+      //       (
+      //         title: const Text("Profile"),
+      //         leading: const Icon(Icons.person_3),
+
+      //         onTap: ()
+      //         {
+      //           Navigator.pop(context);
+      //         }, 
+      //       ),
+
+      //       ListTile(
+      //         title: const Text("LogOut"),
+      //         leading: const Icon(Icons.logout),
+
+      //         onTap: ()
+      //         {
+      //           Navigator.pop(context);
+      //         },
+      //       ),
+        
+      //     ],
+      //   )
+      // ),
+      
+//floating action button
+      floatingActionButton: FloatingActionButton
+      (
+        onPressed: () => setState(() 
+        {
+          null;
+        }),
+        child: Icon(Icons.message),
+        
+      ),
+
+//floating action button location      
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, -> CENTER OF BOTTOM NAVIGATION BAR
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, -> CENTER & ABOVE BOTTOM NAVIGATION BAR
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked, -> RIGHT SIDE & ON BOTTOM NAVIGATION BAR
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat, -> RIGHT SIDE & ABOVE BOTTOM NAVIGATION BAR,DEFAULT
+
+//persistent footer button
+      persistentFooterButtons: <Widget>
+            [
+              Row
+              (
+                children: 
+                [
+                  const SizedBox
+                  (
+                    height: 50,
+                  ),
+
+                  ElevatedButton.icon
+                  (
+                    style: ElevatedButton.styleFrom
+                    (
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+
+                    onPressed: () {},
+                    icon: const Icon
+                    (
+                      Icons.mark_unread_chat_alt,
+                      size: 25.0,
+                    ), 
+                    label: const Text("Unread Messages"),
+                  ),
+
+                  const Spacer(),
+
+                  ElevatedButton.icon
+                  (
+                    style: ElevatedButton.styleFrom
+                    (
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+
+                    onPressed: () {}, 
+                    icon: const Icon
+                    (
+                      Icons.message,
+                      size: 25.0,
+                    ), 
+                    label: const Text("All Messages"),
+                  )
+
+                ],
+              )
+            ],
+
+//bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar
+      (
+        currentIndex: 0,
+        backgroundColor: Colors.blue,
+        fixedColor: Colors.black,
+        items: const 
+        [
+          BottomNavigationBarItem
+          (
+            label: "Personal",
+            icon: Icon(Icons.person),
+          ),
+
+          BottomNavigationBarItem
+          (
+            label: "Group",
+            icon: Icon(Icons.groups),
+          ),
+        ],
+        onTap: (index_of_item) {}  
+
+        //BOTTOM APP BAR
+        // bottomNavigationBar: BottomAppBar
+        // (
+          //  shape: const CircularNotchedRectangle(),
+          //  child: Container
+          //  (
+          //    height: 50.0,
+          //  ),
+        // ),
+      ),
+
     ); 
 
   }
