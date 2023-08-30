bae9fb5331b253ac64128657e8f153bcb2e45768
Vidhya <vidhyaramesh117@gmail.com>
2023-08-30 16:12:39 +0500

    stack till rixktext and text positioned

--- Flutter-scaffold_10/whatsapp/lib/main.dart
+++ Flutter-scaffold_10/whatsapp/lib/main.dart
@@ -56,75 +56,107 @@ class MyHomePageState extends State<MyHomePage>
 //body
       body:  Center
       (
+        child: Stack
+        (  
+          fit: StackFit.passthrough,  
+          // overflow: Overflow.visible,  
+          children: <Widget>
+          [ 
+ 
+          Positioned
+          (
+            top: 30,
+            left: 30,
 
 //rich text
-        child: RichText
-        (
-          text: const TextSpan
+            child: RichText
           (
+            text: const TextSpan
+            (
+              style: TextStyle
+              (
+                color: Colors.grey,
+                fontSize: 15,
+              ),
+              children: <TextSpan>
+              [
+                TextSpan
+                (
+                  text: "Hello",
+                ),
+                TextSpan
+                (
+                  text: " from",
+                ),
+                TextSpan
+                (
+                  text: "WHATSAPP",
+                  style: TextStyle
+                  (
+                    color: Colors.green,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ]
+            ),
+          ),
+          ),
+          
+          Positioned
+          (
+            top: 70,
+            left: 50,
+
+// image
+            child: Column
+            (
+              children: <Widget>
+              [
+                Image.asset('assets/images/wallpaper.jpg'),
+                
+              ],
+            ),
+
+          ),
+
+          const Positioned
+          (
+            bottom: 62,
+            right: 64,
+
+//text
+            child: Text
+          (
+            "Whatsapp",
             style: TextStyle
             (
-              color: Colors.grey,
-              fontSize: 15,
-            ),
-            children: <TextSpan>
-            [
-              TextSpan
-              (
-                text: "Hello",
-              ),
-              TextSpan
-              (
-                text: " from",
-              ),
-              TextSpan
-              (
-                text: "WHATSAPP",
-                style: TextStyle
+              fontSize: 30,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              letterSpacing: 8,
+
+              wordSpacing: 15,
+              // backgroundColor: Color.fromARGB(255, 205, 255, 247),
+              shadows: 
+              [
+                Shadow
                 (
-                  color: Colors.green,
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ]
+                  color: Colors.black,
+                  offset: Offset(2,1),
+                  blurRadius: 15,
+                )
+              ]
+            ),
           ),
+          ),
+          ]
         ),
-//text        
-        // child: Text
-        // (
-        //   "Whatsapp Body",
-        //   style: TextStyle
-        //   (
-        //     fontSize: 30,
-        //     color: Colors.blue,
-        //     fontWeight: FontWeight.bold,
-        //     fontStyle: FontStyle.italic,
-        //     letterSpacing: 8,
+        
+        // 
 
-        //     wordSpacing: 15,
-        //     // backgroundColor: Color.fromARGB(255, 205, 255, 247),
-        //     shadows: 
-        //     [
-        //       Shadow
-        //       (
-        //         color: Colors.black,
-        //         offset: Offset(2,1),
-        //         blurRadius: 15,
-        //       )
-        //     ]
-        //   ),
-        // ),
-
-//image
-        // child: Column
-        // (
-        //   children: <Widget>
-        //   [
-        //     Image.asset('assets/images/wallpaper.jpg'),
-        //   ],
-        // ),
-
+        
       ),
 
 //drawer
