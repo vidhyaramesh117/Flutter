73088d37133b37320d62ce6aafd86e2181f4b5b0
muhsinamehzin@gmail.com <vidhyaramesh117@gmail.com>
2023-09-11 16:32:10 +0500

    till form with textfromfield,radio button

--- whatsapp/lib/screens/page_2.dart
+++ whatsapp/lib/screens/page_2.dart
@@ -1,28 +1,154 @@
 import 'package:flutter/material.dart';
 
-class page_2 extends StatefulWidget {
+class page_2 extends StatefulWidget 
+{
   const page_2({super.key});
 
   @override
   State<page_2> createState() => _page_2State();
 }
 
-class _page_2State extends State<page_2> {
+class _page_2State extends State<page_2> 
+{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
     (
-      appBar: AppBar
+      home: Scaffold
       (
-        title: Text("Page 2"),
-      ),
+        appBar: AppBar
+        (
+          title: Text("Page 2"),
+        ),
 
-      body: Center
-      (
-        child: Column(),
+        body: My_form(),
       ),
     );
-
-
   }
 }
+
+class My_form extends StatefulWidget 
+{ 
+  @override
+  My_form_State createState()
+  {
+    return  My_form_State();
+  }
+}
+
+class My_form_State extends State<My_form>
+{
+  final form_key = GlobalKey<My_form_State>();
+
+  String? gender;
+  @override
+  Widget build(BuildContext context)
+  {
+    return Form
+    (
+      key: form_key,
+      child: Column
+      (
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>
+        [
+          TextFormField
+          (
+            decoration: const InputDecoration
+            (
+              icon: Icon(Icons.person),
+              labelText: "Name :",
+              hintText: "Enter the name",
+            ),
+          ),
+          
+          TextFormField
+          (
+            decoration: const InputDecoration
+            (
+              icon: Icon(Icons.phone),
+              labelText: "Phone :",
+              hintText: "Enter the phone number",
+            ),
+          ),
+
+          Column
+          (
+            children: 
+            [
+              const Padding
+              (
+                padding: EdgeInsets.only(left: 9),
+                child: Text
+                (
+                  "Gender :",
+                  style: TextStyle
+                  (
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+              
+              RadioListTile
+              (
+                title: Text("Male"),
+                value: "male",
+                groupValue: gender,
+                onChanged: (value)
+                {
+                  setState(() 
+                  {
+                    gender = value.toString();
+                  });
+                  
+                },
+              ),
+
+              RadioListTile
+              (
+                title: Text("Female"),
+                value: "female",
+                groupValue: gender,
+                onChanged: (value)
+                {
+                  setState(() 
+                  {
+                    gender = value.toString();
+                  });
+                  
+                },
+              ),
+
+              RadioListTile
+              (
+                title: Text("Other"),
+                value: "other",
+                groupValue: gender,
+                onChanged: (value)
+                {
+                  setState(() 
+                  {
+                    gender = value.toString();
+                  });
+                  
+                },
+              ),
+            ],
+          ),
+
+          Container
+          (
+            child: ElevatedButton
+            (
+              onPressed: () {},
+              child: const Text("Submit"),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
